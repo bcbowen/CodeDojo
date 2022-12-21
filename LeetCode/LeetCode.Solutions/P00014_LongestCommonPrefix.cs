79c0cf136448a0923258a771f0984bfe5c55aa46
Ben Bowen <bcbowen@gmail.com>
2022-12-21 17:55:48 -0500

    Add 2 more problems

---
+++ LeetCode/LeetCode.Solutions/P00014_LongestCommonPrefix.cs
@@ -0,0 +1,44 @@
+﻿using System.Text;
+
+namespace LeetCode.Solutions.P00014_LongestCommonPrefix
+{
+    public class Solution
+    {
+        public string LongestCommonPrefix(string[] strs)
+        {
+            StringBuilder prefix = new StringBuilder();
+            int i = 0;
+            bool done = false;
+
+            while (!done)
+            {
+                if (i >= strs[0].Length)
+                {
+                    done = true;
+                    continue;
+                }
+                char current = strs[0][i];
+                for (int j = 1; j < strs.Length; j++)
+                {
+                    if (i >= strs[j].Length)
+                    {
+                        done = true;
+                        break;
+                    }
+                    if (strs[j][i] != current)
+                    {
+                        done = true;
+                        break;
+                    }
+
+                }
+                if (!done)
+                {
+                    prefix.Append(current);
+                }
+                i++;
+            }
+            return prefix.ToString();
+        }
+    }
+}
