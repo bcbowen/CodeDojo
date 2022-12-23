641139d78459f4b4778f4b87f6c3d0763b06918d
Ben Bowen <bcbowen@gmail.com>
2022-12-23 14:36:27 -0500

    Add problems thru 43

---
+++ LeetCode/LeetCode.Solutions/P00035_SearchInputPosition.cs
@@ -0,0 +1,32 @@
+﻿namespace LeetCode.Solutions.P00035_SearchInputPosition;
+
+public class Solution
+{
+    public int SearchInsert(int[] nums, int target)
+    {
+        if (nums.Length == 1) return target > nums[0] ? 1 : 0;
+        if (target < nums[0]) return 0;
+        if (target > nums[nums.Length - 1]) return nums.Length;
+
+        int i = 0;
+        int j = nums.Length - 1;
+        while (i < j)
+        {
+            int index = (i + j) / 2;
+            if (nums[index] == target)
+            {
+                return index;
+            }
+            else if (nums[index] < target)
+            {
+                i = index + 1;
+            }
+            else
+            {
+                j = index - 1;
+            }
+
+        }
+        return nums[i] < target ? i + 1 : i;
+    }
+}
