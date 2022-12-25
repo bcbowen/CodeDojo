d4385f4656709a97fe74b811e2a91f12022bea04
Ben Bowen <bcbowen@gmail.com>
2022-12-24 22:04:11 -0500

    Add tests through 138

---
+++ LeetCode/LeetCode.Solutions/P00118_PascalsTriangle.cs
@@ -0,0 +1,26 @@
+﻿namespace LeetCode.Solutions.P00118_PascalsTriangle;
+
+public partial class Solution
+{
+    public IList<IList<int>> Generate(int numRows)
+    {
+        IList<IList<int>> triangle = new List<IList<int>>();
+        for (int i = 0; i < numRows; i++)
+        {
+            List<int> row = new List<int>(i + 1);
+
+            row.Add(1);
+            if (i > 1)
+            {
+                for (int j = 1; j < triangle[i - 1].Count; j++)
+                {
+                    row.Add(triangle[i - 1][j - 1] + triangle[i - 1][j]);
+                }
+            }
+            if (i > 0) row.Add(1);
+            triangle.Add(row);
+        }
+
+        return triangle;
+    }
+}
