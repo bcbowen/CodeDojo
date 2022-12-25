bb246b756909fa69a57a219f512a30a58d0dca32
Ben Bowen <bcbowen@gmail.com>
2022-12-24 22:16:26 -0500

    Tests through 139

---
+++ LeetCode/LeetCode/P00139_WordBreak.cs
@@ -0,0 +1,18 @@
+using LeetCode.Solutions.P00139_WordBreak;
+
+namespace LeetCode.Tests.P00139_WordBreak;
+
+public class Tests
+{
+    [Test]
+    [TestCase("leetcode", new[] { "leet", "code" }, true)]
+    [TestCase("applepenapple", new[] { "apple", "pen" }, true)]
+    [TestCase("catsandog", new[] { "cats", "dog", "sand", "and", "cat" }, false)]
+    [TestCase("bb", new[] { "a", "b", "bbb", "bbbb" }, true)]
+    public void Test(string s, IList<string> wordDict, bool expected)
+    {
+        bool result = new Solution().WordBreak(s, wordDict);
+        Assert.AreEqual(expected, result);
+    }
+
+}
