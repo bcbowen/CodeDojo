5642c1d0112a0110e4f8a17df1b808a4e0c2b056
Ben Bowen <bcbowen@gmail.com>
2022-12-25 20:32:21 -0500

    Add problems

--- LeetCode/LeetCode/P000_Template - Copy (25).cs
+++
@@ -1,9 +0,0 @@
-using LeetCode.Solutions.P000_Template;
-
-namespace LeetCode.Tests.P000_Template;
-
-public partial class Tests
-{
-
-
-}
---
+++ LeetCode/LeetCode/P00144_BinaryTreePreOrder.cs
@@ -0,0 +1,9 @@
+using LeetCode.Solutions.P00144_BinaryTreePreOrder;
+
+namespace LeetCode.Tests.P00144_BinaryTreePreOrder;
+
+public class Tests
+{
+
+
+}
