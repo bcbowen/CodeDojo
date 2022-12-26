ba8b9811b2777d102668443eb40b81f4572b23fd
Ben Bowen <bcbowen@gmail.com>
2022-12-26 18:43:32 -0500

    Add additional problems thru 234

---
+++ LeetCode/LeetCode.Solutions/P00215_KthLargestElementInArray.cs
@@ -0,0 +1,21 @@
+﻿namespace LeetCode.Solutions.P00215_KthLargestElementInArray;
+
+public partial class Solution
+{
+    public int FindKthLargest(int[] nums, int k)
+    {
+        PriorityQueue<int, int> q = new PriorityQueue<int, int>();
+        foreach (int num in nums)
+        {
+            q.Enqueue(num, -num);
+        };
+
+        int result = 0;
+        for (int i = 0; i < k; i++)
+        {
+            result = q.Dequeue();
+        }
+
+        return result;
+    }
+}
---
+++ LeetCode/LeetCode/P00217_ContainsDupes.cs
@@ -0,0 +1,16 @@
+using LeetCode.Solutions.P00217_ContainsDupes;
+
+namespace LeetCode.Tests.P00217_ContainsDupes;
+
+public class Tests
+{
+    [TestCase(new[] { 1, 2, 3, 1 }, true)]
+    [TestCase(new[] { 1, 2, 3, 4 }, false)]
+    [TestCase(new[] { 1, 1, 1, 3, 3, 4, 3, 2, 4, 2 }, true)]
+    public void TestContainsDupes(int[] nums, bool expected)
+    {
+        bool result = new Solution().ContainsDuplicate(nums);
+        Assert.AreEqual(expected, result);
+    }
+
+}
