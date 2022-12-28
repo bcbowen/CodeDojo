22c5744169a123a67d5c29e8d0072b9c40993e22
Ben Bowen <bcbowen@gmail.com>
2022-12-28 08:09:30 -0500

    Refactor 14 which was pretty nasty

--- LeetCode/LeetCode/P00014_LongestCommonPrefix.cs
+++ LeetCode/LeetCode/P00014_LongestCommonPrefix.cs
@@ -7,6 +7,9 @@ public class Tests
     [Test]
     [TestCase(new string[] { "flower", "flow", "flight" }, "fl")]
     [TestCase(new string[] { "dog", "racecar", "car" }, "")]
+    [TestCase(new string[] { "dog", "", "dork" }, "")]
+    [TestCase(new string[] { "", "racecar", "rat" }, "")]
+    [TestCase(new string[] { "", "" }, "")]
     public void Test(string[] input, string expected)
     {
         string result = new Solution().LongestCommonPrefix(input);
