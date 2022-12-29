03a226f7950f889b566d7b23ddbdf0b2e8d65cc1
Ben Bowen <bcbowen@gmail.com>
2022-12-28 20:22:32 -0500

    added problems thru 250

---
+++ LeetCode/LeetCode.Solutions/P00236_LowestCommonAncestorBinaryTree.cs
@@ -0,0 +1,34 @@
+﻿using LeetCode.Solutions.Models.Tree.BinaryTree;
+
+namespace LeetCode.Solutions.P00236_LowestCommonAncestorBinaryTree;
+
+public partial class Solution
+{
+    public TreeNode LowestCommonAncestor(TreeNode root, TreeNode p, TreeNode q)
+    {
+        (int count, TreeNode node) = FindAncestor(root, p.val, q.val);
+        return node;
+    }
+
+
+    internal (int, TreeNode) FindAncestor(TreeNode root, int p, int q)
+    {
+        if (root == null) return (0, null);
+
+        int matchCount = 0;
+        int count;
+        TreeNode ancestor = null;
+        (count, ancestor) = FindAncestor(root.left, p, q);
+        if (ancestor != null) return (count, ancestor);
+        matchCount += count;
+
+        (count, ancestor) = FindAncestor(root.right, p, q);
+        if (ancestor != null) return (count, ancestor);
+        matchCount += count;
+
+        if (root.val == p || root.val == q) matchCount++;
+
+        return (matchCount, matchCount >= 2 ? root : null);
+
+    }
+}
