63a14dd0fe1de0e8342b12c23d27f6e2aa974bce
Ben Bowen <bcbowen@gmail.com>
2023-01-01 20:40:37 -0500

    Problems thru 590

---
+++ LeetCode/LeetCode.Solutions/P00589_PreorderTreeTraversal_DFS.cs
@@ -0,0 +1,20 @@
+﻿namespace LeetCode.Solutions.P00589_PreorderTreeTraversal_DFS;
+using LeetCode.Solutions.Models.Tree.NAryTree;
+
+public class Solution
+{
+    public IList<int> Preorder(Node root)
+    {
+        List<int> values = new List<int>();
+        if (root == null) return values;
+        values.Add(root.val);
+        if (root.children != null)
+        {
+            foreach (Node child in root.children)
+            {
+                values.AddRange(Preorder(child));
+            }
+        }
+        return values;
+    }
+}
---
+++ LeetCode/LeetCode/P00566_ReshapeMatrix.cs
@@ -0,0 +1,39 @@
+using LeetCode.Solutions.P00566_ReshapeMatrix;
+
+namespace LeetCode.Tests.P00566_ReshapeMatrix;
+
+public class Tests
+{
+    [TestCase(1, 4, new[] { 1, 2 }, new[] { 3, 4 })]
+    [TestCase(2, 2, new[] { 1, 2 }, new[] { 3, 4 })]
+    [TestCase(2, 3, new[] { 1, 2, 3 }, new[] { 4, 5, 6 })]
+    [TestCase(3, 2, new[] { 1, 2, 3 }, new[] { 4, 5, 6 })]
+    public void ReshapeTestValidData(int r, int c, params int[][] matrix)
+    {
+        int[][] result = new Solution().MatrixReshape(matrix, r, c);
+        Assert.AreEqual(r, result.Length);
+        Assert.AreEqual(c, result[0].Length);
+    }
+
+    [Theory]
+    [TestCase(2, 4, new[] { 1, 2 }, new[] { 3, 4 })]
+    public void ReshapeTestInalidData(int r, int c, params int[][] matrix)
+    {
+        int[][] result = new Solution().MatrixReshape(matrix, r, c);
+        Assert.AreEqual(matrix, result);
+    }
+
+
+    [TestCase(new[] { 1, 2 }, new[] { 1 }, new[] { 2 })]
+    [TestCase(new[] { 1, 2, 3, 4 }, new[] { 1, 2 }, new[] { 3, 4 })]
+    [TestCase(new[] { 1, 2, 3, 4 }, new[] { 1, 2, 3, 4 })]
+    [TestCase(new[] { 1, 2, 3, 4, 5, 6 }, new[] { 1, 2, 3 }, new[] { 4, 5, 6 })]
+    [TestCase(new[] { 1, 2, 3, 4, 5, 6, 7, 8, 9 }, new[] { 1, 2, 3 }, new[] { 4, 5, 6 }, new[] { 7, 8, 9 })]
+    public void FlattenTest(int[] expected, params int[][] matrix)
+    {
+        int[] result = new Solution().Flatten(matrix);
+        Assert.AreEqual(expected, result);
+    }
+
+
+}
