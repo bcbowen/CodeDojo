ee68e64bd1325bfbff8f97396954aae49114d891
Ben Bowen <bcbowen@gmail.com>
2023-01-03 21:12:32 -0500

    Problems thru 1137

---
+++ LeetCode/LeetCode.Solutions/P00976_LargestPerimeterTriangle.cs
@@ -0,0 +1,25 @@
+﻿namespace LeetCode.Solutions.P00976_LargestPerimeterTriangle;
+
+public class Solution
+{
+    public int LargestPerimeter(int[] nums)
+    {
+        if (nums == null || nums.Length < 3) return 0;
+        int p = 0;
+        Array.Sort(nums);
+        int i = nums.Length - 1;
+        while (i > 1)
+        {
+            int a = nums[i - 2];
+            int b = nums[i - 1];
+            int c = nums[i];
+            if (a + b > c)
+            {
+                p = a + b + c;
+                break;
+            }
+            i--;
+        }
+        return p;
+    }
+}
