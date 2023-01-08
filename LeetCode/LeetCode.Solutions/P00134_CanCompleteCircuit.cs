fdb6d08d115b576a6b721fd33eab2ce1834aeb37
Ben Bowen <bcbowen@gmail.com>
2023-01-08 08:27:41 -0500

    Fix test for 15

--- LeetCode/LeetCode.Solutions/P00134_CanCompleteCircuit.cs
+++ LeetCode/LeetCode.Solutions/P00134_CanCompleteCircuit.cs
@@ -2,25 +2,8 @@
 
 public class Solution
 {
-    //private int[] _gas;
-    //private int[] _cost; 
-
     public int CanCompleteCircuit(int[] gas, int[] cost)
     {
-        /*
-        if (gas == null || cost == null || gas.Length == 0 || gas.Length != cost.Length) return -1;
-
-        //_gas = gas;
-        //_cost = cost;
-        for (int i = 0; i < gas.Length; i++) 
-        {
-            if (_cost[i] == 0 && _gas[i] == 0) continue;
-            if (CompletesCircuit(i)) return i;
-        }
-
-        return -1;
-        */
-
         int totalTank = 0;
         int currentTank = 0;
         int startingStation = 0;
@@ -38,23 +21,4 @@ public class Solution
         }
         return totalTank >= 0 ? startingStation : -1;
     }
-
-    /*
-    private bool CompletesCircuit(int i)
-    {
-        Func<int, int> nextIndex = (int currentIndex) => currentIndex < _gas.Length - 1 ? currentIndex + 1 : 0;
-        int energy = 0;
-        int index = i;
-        do 
-        {
-            energy += _gas[index];
-            energy -= _cost[index];
-            if (energy < 0) return false;
-            index = nextIndex(index);
-        } 
-        while (index != i);
-
-        return true;
-    }
-    */
 }
