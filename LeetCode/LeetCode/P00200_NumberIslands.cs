cc0f9a5b7723160e1a13149105042faee6b3c52a
Ben Bowen <bcbowen@gmail.com>
2023-01-11 23:19:33 -0500

    Add problem 200 Number Islands

---
+++ LeetCode/LeetCode/P00200_NumberIslands.cs
@@ -0,0 +1,54 @@
+using LeetCode.Solutions.P00200_NumberIslands;
+
+namespace LeetCode.Tests.P00200_NumberIslands;
+
+public class Tests
+{
+    [TestCase(1, 
+        new char[] { '1', '1', '1', '1', '0' },
+        new char[] { '1', '1', '0', '1', '0' }, 
+        new char[] { '1', '1', '0', '0', '0' }, 
+        new char[] { '0', '0', '0', '0', '0' } ), ]
+    [TestCase(3,
+        new char[] { '1', '1', '0', '0', '0' },
+        new char[] { '1', '1', '0', '0', '0' },
+        new char[] { '0', '0', '1', '0', '0' },
+        new char[] { '0', '0', '0', '1', '1' }),]
+    [TestCase(1,
+        new char[] { '1' }),]
+    [TestCase(1,
+        new char[] { '1', '1', '1' },
+        new char[] { '0', '1', '0' },
+        new char[] { '1', '1', '1' }),]
+    [TestCase(1,
+        new char[] { '1', '0', '1', '1', '1' },
+        new char[] { '1', '0', '1', '0', '1' },
+        new char[] { '1', '1', '1', '0', '1' }),]
+
+    // [["1","0","1","1","1"],["1","0","1","0","1"],["1","1","1","0","1"]]
+    public void NumberIslandTest(int expected, params char[][] grid) 
+    { 
+        int result = new Solution().NumIslands(grid);
+        Assert.That(result, Is.EqualTo(expected));
+    }
+    /*
+    Example 1:
+Input: grid = [
+  ["1","1","1","1","0"],
+  ["1","1","0","1","0"],
+  ["1","1","0","0","0"],
+  ["0","0","0","0","0"]
+]
+Output: 1
+
+Example 2:
+Input: grid = [
+  ["1","1","0","0","0"],
+  ["1","1","0","0","0"],
+  ["0","0","1","0","0"],
+  ["0","0","0","1","1"]
+]
+Output: 3
+    */
+
+}
