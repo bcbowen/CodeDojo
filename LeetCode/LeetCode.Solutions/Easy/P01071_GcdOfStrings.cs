8eb0b8eeb5845951cb1f738f0865f406d354deca
Ben Bowen <bcbowen@gmail.com>
2023-02-02 16:34:08 -0500

    Add 003

---
+++ LeetCode/LeetCode.Solutions/Easy/P01071_GcdOfStrings.cs
@@ -0,0 +1,35 @@
+﻿using System.Text;
+
+namespace LeetCode.Solutions.Easy.P01071_GcdOfStrings;
+
+public partial class Solution
+{
+    public string GcdOfStrings(string str1, string str2)
+    {
+        StringBuilder gcdBuilder = new StringBuilder(str1[0]);
+        string gcd = "";
+        int i = 0;
+        while (i <= str2.Length / 2) 
+        {
+            if (CheckGcd(gcdBuilder.ToString(), str2))
+            { 
+                gcd = gcdBuilder.ToString();
+            }
+            gcdBuilder.Append(str2[++i]);
+        }
+        return gcd;
+    }
+
+    internal bool CheckGcd(string gcd, string value) 
+    {
+        if (value.Length % gcd.Length != 0) return false;
+
+        int i = 0;
+        while (i < value.Length - 1) 
+        {
+            if (gcd != value.Substring(i, gcd.Length)) return false;
+            i += gcd.Length;
+        }
+        return true;
+    }
+}
---
+++ LeetCode/LeetCode/Easy/P01071_GcdOfStrings.cs
@@ -0,0 +1,28 @@
+using LeetCode.Solutions.Easy.P01071_GcdOfStrings;
+
+namespace LeetCode.Tests.Easy.P01071_GcdOfStrings;
+[TestFixture]
+[Category("P01071")]
+[Category("Easy")]
+public class Tests
+{
+    /*
+    Example 1:
+    Input: str1 = "ABCABC", str2 = "ABC"
+    Output: "ABC"
+
+    Example 2:
+    Input: str1 = "ABABAB", str2 = "ABAB"
+    Output: "AB"
+    
+    Example 3:
+    Input: str1 = "LEET", str2 = "CODE"
+    Output: ""
+
+    Example 4:
+    Input: str1 = "ABABABAB", str2 = "ABAB"
+    Output: "ABAB"
+
+    */
+
+}
