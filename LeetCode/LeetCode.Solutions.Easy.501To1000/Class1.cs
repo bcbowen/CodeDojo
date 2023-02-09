8d5829c1d1bc8dc4635599fb8c804bf0f9b76ead
Ben Bowen <bcbowen@gmail.com>
2023-02-09 09:37:49 -0500

    Separate solutions for hard

--- LeetCode/LeetCode.Solutions.Easy.501To1000/Class1.cs
+++
@@ -1,7 +0,0 @@
-﻿namespace LeetCode.Solutions.Easy._501To1000
-{
-    public class Class1
-    {
-
-    }
-}
--- LeetCode/LeetCode.Tests.Easy.501To1000/Class1.cs
+++
@@ -1,7 +0,0 @@
-﻿namespace LeetCode.Tests.Easy._501To1000
-{
-    public class Class1
-    {
-
-    }
-}
