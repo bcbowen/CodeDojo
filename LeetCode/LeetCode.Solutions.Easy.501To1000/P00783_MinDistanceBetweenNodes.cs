a423ca28c3c5119f7f96a47d5036f0e21e7d77ee
Ben Bowen <bcbowen@gmail.com>
2023-02-17 15:47:29 -0500

    Add 783

---
+++ LeetCode/LeetCode.Solutions.Easy.501To1000/P00783_MinDistanceBetweenNodes.cs
@@ -0,0 +1,32 @@
+﻿using LeetCode.Models.Tree.BinaryTree;
+
+namespace LeetCode.Solutions.Easy.P00783_MinDistanceBetweenNodes;
+
+public class Solution
+{
+    public int MinDiffInBST(TreeNode root)
+    {
+        List<int> inorderValues = new List<int>();
+        GetInorderValues(root, inorderValues);
+        int min = inorderValues[1] - inorderValues[0];
+        for (int i = 2; i < inorderValues.Count; i++) 
+        {
+            min = Math.Min(min, inorderValues[i] - inorderValues[i - 1]);
+        }
+        return min;
+    }
+
+    internal void GetInorderValues(TreeNode root, List<int> values)
+    {
+        if (root == null) return;
+        if (root.left != null) 
+        {
+            GetInorderValues(root.left, values);
+        }
+        values.Add(root.val);
+        if (root.right != null) 
+        {
+            GetInorderValues(root.right, values);
+        }
+    }
+}
