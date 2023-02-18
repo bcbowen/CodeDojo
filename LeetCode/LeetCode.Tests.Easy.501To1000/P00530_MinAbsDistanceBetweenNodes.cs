30ba8470f3ef80b4555548c20a154f30bb39ef97
Ben Bowen <bcbowen@gmail.com>
2023-02-18 12:51:50 -0500

    Add 530

---
+++ LeetCode/LeetCode.Tests.Easy.501To1000/P00530_MinAbsDistanceBetweenNodes.cs
@@ -0,0 +1,41 @@
+using LeetCode.Solutions.Easy.P00530_MinAbsDistanceBetweenNodes;
+using LeetCode.Models.Tree.BinaryTree;
+using static LeetCode.Models.Tree.BinaryTree.TreeNodeExtensions;
+namespace LeetCode.Tests.Easy.P00530_MinAbsDistanceBetweenNodes;
+
+[TestFixture]
+[Category("Easy")]
+public class Tests
+{
+    [TestCase(new[] { 4, 2, 6, 1, 3}, 1)]
+    [TestCase(new[] { 1, 0, 48, 12, 49 }, 1)]
+    public void P00783_Test(int[] values, int expected) 
+    {
+        TreeNode root = null;
+        foreach (int value in values)
+        {
+            root = root.AddNode(value);    
+        }
+        int result = new Solution().GetMinimumDifference(root);
+        Assert.That(result, Is.EqualTo(expected));
+
+    }
+
+    [TestCase(new[] { 4, 2, 6, 1, 3 }, new[] { 1, 2, 3, 4, 6} )]
+    [TestCase(new[] { 1, 0, 48, 12, 49 }, new[] { 0, 1, 12, 48, 49 })]
+    public void P00783_Test(int[] values, int[] expected)
+    {
+        TreeNode root = null;
+        foreach (int value in values)
+        {
+            root = root.AddNode(value);
+        }
+        List<int> result = new List<int>();
+        new Solution().GetInorderValues(root, result);
+        Assert.That(result, Is.EqualTo(expected.ToList()));
+    }
+
+
+
+
+}
