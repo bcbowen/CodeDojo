46e518a905790abb42cf725c48b963ea3eecc5d5
Ben Bowen <bcbowen@gmail.com>
2023-06-22 22:59:12 -0400

    Winforms solver

---
+++ leetcode/Hard/37SudokuSolver/Sudokamatic/Sudokamatic/frmMain.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Sudokamatic
+{
+    public partial class frmMain : Form
+    {
+        public frmMain()
+        {
+            InitializeComponent();
+        }
+
+        private void btnClose_Click(object sender, EventArgs e)
+        {
+            Close(); 
+        }
+
+        private void textBox12_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void textBox60_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
