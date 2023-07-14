70bc9ea6275533905c02fd5b6677828ea7481515
Ben Bowen <bcbowen@gmail.com>
2023-07-14 17:15:24 -0400

    Fix tests for loading sudoku board from chars and string

--- leetcode/Hard/37SudokuSolver/Sudokamatic/SudokerSolverTests/SudokuBoardFromCharArrayTests.cs
+++ leetcode/Hard/37SudokuSolver/Sudokamatic/SudokerSolverTests/SudokuBoardFromCharArrayTests.cs
@@ -2,6 +2,7 @@ using SudokerSolver;
 
 namespace SudokerSolverTests; 
 
+[TestFixture]
 public class SudokuBoardFromCharArrayTests
 {
     private SudokuBoard? _board = null; 
@@ -21,10 +22,19 @@ public class SudokuBoardFromCharArrayTests
             new []{'.','.','.','4','1','9','.','.','5'},
             new []{'.','.','.','.','8','.','.','7','9'}
         };
-        SudokuBoard _board = SudokuBoard.Create(chars);
+        _board = SudokuBoard.Create(chars);
     }
 
     [TestCase(0, 0, 5)]
+    [TestCase(0, 1, 3)]
+    [TestCase(0, 2, 0)]
+    [TestCase(0, 4, 7)]
+    [TestCase(2, 1, 9)]
+    [TestCase(3, 0, 8)]
+    [TestCase(8, 0, 0)]
+    [TestCase(8, 4, 8)]
+    [TestCase(8, 7, 7)]
+    [TestCase(8, 8, 9)]
     public void BoardPopulatedWithExpectedValues(int row, int col, int expected)
     {
         int value = _board.Board[row][col].Value;
