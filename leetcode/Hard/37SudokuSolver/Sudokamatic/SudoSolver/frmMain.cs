d94503239b8eac5ec836d73d9ea458a01422fea6
Ben Bowen <bcbowen@gmail.com>
2023-07-16 22:38:33 -0400

    Work on sudoku form

--- leetcode/Hard/37SudokuSolver/Sudokamatic/SudoSolver/frmMain.cs
+++ leetcode/Hard/37SudokuSolver/Sudokamatic/SudoSolver/frmMain.cs
@@ -28,17 +28,21 @@ namespace SudoSolver
         {
             int x = 20;
             int y = 20;
-            int spacer = 10; 
+            int spacer = 10;
+            int height = CellWidth; 
             for (int row = 0; row < 9; row++) 
             {
                 for (int col = 0; col < 9; col++) 
                 {
-                    grpBoard.Controls.Add(InitCell(x, y, $"txt{row}_{col}"));
-                    x += CellWidth + spacer; 
+                    TextBox textBox = InitCell(x, y, $"txt{row}_{col}");
+                    grpBoard.Controls.Add(textBox);
+                    x += CellWidth + spacer;
+                    height = textBox.Height;
                 }
-                y += CellWidth + spacer;
+                y += height + spacer;
                 x = 20; 
             }
+            
              
         }
 
@@ -59,6 +63,56 @@ namespace SudoSolver
                 Font = font
             };
             return textBox;
+        }
+
+        private void frmMain_Paint(object sender, PaintEventArgs e)
+        {
+            
+        }
+
+        private void grpBoard_Paint(object sender, PaintEventArgs e)
+        {
+            Graphics graphics = e.Graphics;
+            Pen pen = new Pen(Color.Black, 2);
+            TextBox textBox = (TextBox)grpBoard.Controls["txt0_2"];
+            int startY = textBox.Top;
+
+            textBox = (TextBox)grpBoard.Controls["txt8_2"];
+            int endY = textBox.Top + textBox.Height;
+            int x = textBox.Left + textBox.Width + 5;
+            graphics.DrawLine(pen, x, startY, x, endY);
+
+            textBox = (TextBox)grpBoard.Controls["txt0_5"];
+            x = textBox.Left + textBox.Width + 5;
+            graphics.DrawLine(pen, x, startY, x, endY);
+
+            textBox = (TextBox)grpBoard.Controls["txt0_0"];
+            x = textBox.Left;
+            graphics.DrawLine(pen, x, startY, x, endY);
+
+            textBox = (TextBox)grpBoard.Controls["txt0_8"];
+            x = textBox.Left + textBox.Width;
+            graphics.DrawLine(pen, x, startY, x, endY);
+
+            textBox = (TextBox)grpBoard.Controls["txt0_0"];
+            int startX = textBox.Left;
+            textBox = (TextBox)grpBoard.Controls["txt0_8"];
+            int endX = textBox.Left + textBox.Width;
+            int y = textBox.Top;
+            graphics.DrawLine(pen, startX, y, endX, y);
+
+            textBox = (TextBox)grpBoard.Controls["txt2_0"];
+            y = textBox.Top + textBox.Height;
+            graphics.DrawLine(pen, startX, y, endX, y);
+
+            textBox = (TextBox)grpBoard.Controls["txt5_0"];
+            y = textBox.Top + textBox.Height + 5;
+            graphics.DrawLine(pen, startX, y, endX, y);
+
+            textBox = (TextBox)grpBoard.Controls["txt8_0"];
+            y = textBox.Top + textBox.Height + 5;
+            graphics.DrawLine(pen, startX, y, endX, y);
+
         }
     }
 }
