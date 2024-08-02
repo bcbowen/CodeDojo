ac29b9c3af1f858f68b95726d1997bd810797220
Ben Bowen <bcbowen@gmail.com>
2024-08-01 21:39:11 -0400

    2206

--- leetcode/Easy/1636_SortArrayByIncreasingFrequency.py
+++ leetcode/Easy/1636_SortArrayByIncreasingFrequency.py
@@ -30,14 +30,6 @@ Output: [1,3,3,2,2]
 Explanation: '2' and '3' both have a frequency of 2, so they are sorted in decreasing order.
 """
 
-# nums, outputs
-"""
-run_cases = [
-    ([1,1,2,2,2,3], [3,1,1,2,2,2]),
-    ([2,3,1,3,2],[1,3,3,2,2])
-]
-"""
-
 @pytest.mark.parametrize("nums, expected", [
     ([1,1,2,2,2,3], [3,1,1,2,2,2]),
     ([2,3,1,3,2], [1,3,3,2,2])
