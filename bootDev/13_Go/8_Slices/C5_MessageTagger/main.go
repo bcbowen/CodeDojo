abd093ddd54445bdfc2618a99e5f989de4a80574
Ben Bowen <bcbowen@gmail.com>
2024-09-09 00:26:28 -0400

    Reorganize boot dev exercise files

---
+++ bootDev/13_Go/8_Slices/C5_MessageTagger/main.go
@@ -0,0 +1,28 @@
+package main
+import "strings"
+
+type sms struct {
+	id      string
+	content string
+	tags    []string
+}
+
+func tagMessages(messages []sms, tagger func(sms) []string) []sms {
+	for _, message := range messages {
+		message.tags = tagger(message)
+	}
+	
+	return messages
+}
+
+func tagger(msg sms) []string {
+	tags := []string{}
+	content := strings.ToLower(msg.content)
+	if strings.Contains(content, "urgent") {
+		tags = append(tags, "Urgent")
+	}
+	if strings.Contains(content, "sale") {
+		tags = append(tags, "Promo")
+	}
+	return tags
+}
