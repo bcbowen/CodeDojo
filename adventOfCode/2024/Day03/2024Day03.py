71b98092dcd2c335411ff37e8dd11e3e86d34800
bcbowen <bcbowen@gmail.com>
2024-12-03 11:43:15 -0500

    Day 3 part 1 done

--- adventOfCode/2024/Day03/2024Day03.py
+++ adventOfCode/2024/Day03/2024Day03.py
@@ -1,8 +1,9 @@
 import pytest
+import re
 
 from pathlib import Path
 
-def get_input_filepath(file_name: str): 
+def get_input_filepath(file_name: str) -> str: 
     current_path = Path(__file__).parent
     day = current_path.name
     current_path = current_path.parent
@@ -14,3 +15,40 @@ def get_input_filepath(file_name: str):
     input_path = private_files_base / year / day / file_name
     return input_path
 
+def get_input(file_name: str) -> str: 
+    path = get_input_filepath(file_name)
+    with open(path, "r") as file: 
+        input = file.read()
+    
+    return input
+
+
+def get_total(file_name : str) -> int: 
+    #pattern = r"(mul\((\d+),(\d+)\))"
+    pattern = r"mul\((\d+),(\d+)\)"
+    input = get_input(file_name)
+    #matches = re.match(pattern, input)
+    total = 0
+    for match in re.finditer(pattern, input): 
+        num1, num2 = map(int, match.groups())
+        total += num1 * num2
+    return total
+
+def main(): 
+    file_name = "sample.txt"
+    result = get_total(file_name)
+    print(f"Part 1 sample: {result}")
+    file_name = "input.txt"
+    result = get_total(file_name)
+    print(f"Part 1 result: {result}")
+
+
+def test_part1(): 
+    expected = 161
+    file_name = "sample.txt"
+    result = get_total(file_name)
+    assert(expected == result)
+
+if __name__ == "__main__": 
+    pytest.main([__file__])
+    main()
