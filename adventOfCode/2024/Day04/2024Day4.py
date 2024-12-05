fabbdd865ce7c3edc5b7331df9f8c93a68c021f9
bcbowen <bcbowen@gmail.com>
2024-12-04 20:18:07 -0500

    Working on day 4... wip

--- adventOfCode/2024/Day04/2024Day4.py
+++ adventOfCode/2024/Day04/2024Day4.py
@@ -13,16 +13,65 @@ def get_input_filepath(file_name: str):
     input_path = private_files_base / year / day / file_name
     return input_path
 
+def load_input(file_name: str) -> list[list[str]]: 
+    path = get_input_filepath(file_name)
+    with open(path, "r") as file: 
+        grid = [list(line) for line in file.readlines()]
+
+    return grid
+
+"""
+from collections import namedtuple
+
+# Define a namedtuple to represent the direction
+Direction = namedtuple('Direction', ['x', 'y'])
+
+# Declare constants using named tuples
+RTL = Direction(0, 1)
+LTR = Direction(0, -1)
+D = Direction(-1, 0)
+U = Direction(1, 0)
+
+# Access the values using named attributes
+print(RTL.x, RTL.y)  # Output: 0 1
+"""
+
+def part1(file_name: str) -> int: 
+    grid = load_input(file_name) 
+    found = 0
+    for row in range(len(grid)): 
+        for col in range(len(row)): 
+            if grid[col][row] == 'X': 
+                # E
+
+    return found
+
+def part2(file_name: str) -> int: 
+    return -1
+
+def test1(): 
+    file_name = "sample.txt"
+    expected = 18
+    result = part1(file_name)
+    assert(result == expected)
+
+
+def test2(): 
+    file_name = "sample.txt"
+    expected = -1
+    result = part2(file_name)
+    assert(result == expected)
+
 def main():
     # part 1:
-    result = day2("sample.txt", 0)
+    result = part1("sample.txt")
     print(f"Sample part1: {result}")
-    result = day2("input.txt", 0)
+    result = part1("input.txt")
     print(f"Part1: {result}")
     # part 2:
-    result = day2("sample.txt", 1)
+    result = part2("sample.txt")
     print(f"Sample part2: {result}")
-    result = day2("input.txt", 1)
+    result = part2("input.txt")
     print(f"Part2: {result}")
 
 if __name__ == "__main__": 
