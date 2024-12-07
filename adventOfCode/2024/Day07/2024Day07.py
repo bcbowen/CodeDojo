83537d827c6e3db0106052bd459b6da1b13658b7
Ben Bowen <bcbowen@gmail.com>
2024-12-07 09:57:17 -0500

    Working on day 7 part1... not working yet

--- adventOfCode/2024/Day07/2024Day07.py
+++ adventOfCode/2024/Day07/2024Day07.py
@@ -1,5 +1,6 @@
 import pytest
 from pathlib import Path
+from itertools import product
 
 def get_input_filepath(file_name: str):
     current_path = Path(__file__).parent
@@ -24,18 +25,63 @@ def load_input(file_name: str) -> dict[int, list[int]]:
     return input
 
 
-def part1(): 
-    pass
+def get_ops(len: int) -> list[str]: 
+    op_list = ['+', '*']
+    return [p for p in product(op_list, repeat=len)]
+
+def check_values(total: int, values: list[int]) -> bool: 
+    for op_set in get_ops(len(values) - 1): 
+        running_total = values[0]
+        
+        for i in range(1, len(values)): 
+            if op_set[i - 1] == '+': 
+                running_total += values[i]
+            else: 
+                running_total *= values[i]
+        if running_total == total: 
+            return True
+    return False
+
+def part1(file_name: str) -> int: 
+    input = load_input(file_name)
+    total = 0
+    for key in input: 
+        if check_values(key, input[key]): 
+            total += key
+
+    return total
 
 def test_part1(): 
-    pass
+    expected = 3749
+    result = part1("sample.txt")
+    assert(result == expected)
 
-def test_part2(): 
+def test_part2(file_name: str): 
     pass
 
 
 def main(): 
-    pass
+    
+    # part 1:
+    result = part1("sample.txt")
+    print(f"Sample part1: {result}")
+    
+    result = part1("input.txt")
+    print(f"Part1: {result}")
+    
+    
+    """
+    # part 2:
+    result = part2("sample.txt")
+    print(f"Sample part2: {result}")
+    result = part2("input.txt")
+    print(f"Part2: {result}")
+    """
+
+def test_part1(): 
+    expected = 3749
+    result = part1("sample.txt")
+    assert (expected == result)
 
 def test_input_load(): 
     input = load_input("sample.txt")
