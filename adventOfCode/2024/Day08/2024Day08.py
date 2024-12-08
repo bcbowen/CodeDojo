ea42a808f10102a0a255a6afd8e9ddeab8fcbb85
bcbowen <bcbowen@gmail.com>
2024-12-08 13:19:17 -0500

    Day8 part 1 done

--- adventOfCode/2024/Day08/2024Day08.py
+++ adventOfCode/2024/Day08/2024Day08.py
@@ -39,54 +39,57 @@ def map_stations(grid: list[list[str]]) -> dict[str, list[tuple[int, int]]]:
 for each station, map the antinode locations and return them along with a total count
 """
 def map_antinodes(station_map : dict[str, list[tuple[int, int]]], grid_size : int) -> tuple[dict[str, list[tuple[int, int]], int]]: 
-    total = 0
+    all_nodes = set()
     node_map = {}
 
     """
     for a pair of stations, calculate the antinode positions and return the ones that are in bounds
     """
-    def calc_antinodes (a: tuple[int, int], b: tuple[int, int], grid_size) -> list[tuple[int, int]]:  
-        top = a if a[0] < b[0] else b
-        bottom = b if top == a else a 
-
-        dy = bottom[0] - top[0]
-        dx = bottom[1] - bottom[1]
-        nodes = []
-        node1 = (top[0] - dy, top[1] - dx)
+    def calc_antinodes (a: tuple[int, int], b: tuple[int, int], grid_size) -> set[tuple[int, int]]:  
+        
+        dy = b[0] - a[0]
+        dx = b[1] - a[1]
+        nodes = set()
+        node1 = (a[0] - dy, a[1] - dx)
         if node1[0] >= 0 and node1[0] < grid_size and node1[1] >= 0 and node1[1] < grid_size: 
-            nodes.append(node1)
+            nodes.add(node1)
+            all_nodes.add(node1)
 
-        node2 = (top[0] - dy, top[1] - dx)
+        node2 = (b[0] + dy, b[1] + dx)
         if node2[0] >= 0 and node2[0] < grid_size and node2[1] >= 0 and node2[1] < grid_size: 
-            nodes.append(node2)
+            nodes.add(node2)
+            all_nodes.add(node2)
 
         return nodes
     
     for station_key in station_map:
         node_map[station_key] = []
         for i in range(len(station_map[station_key]) -1): 
-            for j in range(1, len(station_map[station_key])): 
+            for j in range(i + 1, len(station_map[station_key])): 
                 nodes = calc_antinodes(station_map[station_key][i], station_map[station_key][j], grid_size) 
-                total += len(nodes)
                 node_map[station_key].extend(nodes)
 
 
 
-    return (node_map, total)
+    return (node_map, len(all_nodes))
 
 def part1(file_name: str) -> int: 
     grid = load_grid(file_name)
-    print('stations:')
+    #print('stations:')
     station_map = map_stations(grid)
+    
+    _, node_count = map_antinodes(station_map, len(grid))
+    """
     for key in station_map:
         print(f"Station {key}") 
         for node in station_map[key]: 
             print(node)
-    node_map, node_count = map_antinodes(station_map, len(grid))
+    
     for key in node_map: 
         print(f"Station {key} nodes")
         for node in node_map[key]: 
             print(node)
+    """            
     return node_count
 
 
@@ -109,6 +112,13 @@ def test_part1():
     result = part1("sample.txt")
     assert(result == expected)
 
+def test_calc_antinodes(): 
+    stations = {}
+    stations[0] = [(1, 8),(2, 5)]
+    result, _ = map_antinodes(stations, 12) 
+    assert((0, 11) in result[0])
+    assert((3, 2) in result[0])
+
 def main(): 
     # part 1:
     result = part1("sample.txt")
@@ -130,3 +140,4 @@ def main():
 
 if __name__ == "__main__": 
     pytest.main([__file__])
+    main()
