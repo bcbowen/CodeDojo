3b795dacc1ed8f336af1ea2fd24ebe786296d7de
Ben Bowen <bcbowen@gmail.com>
2024-12-15 00:46:51 -0500

    Setup days 14 and 15

---
+++ adventOfCode/2024/Day14/2024Day14.py
@@ -0,0 +1,23 @@
+import pytest
+from pathlib import Path
+
+
+def get_input_filepath(file_name: str):
+    current_path = Path(__file__).parent
+    day = current_path.name
+    current_path = current_path.parent
+    year = current_path.name
+
+    # traverse up directories to the private files
+    private_files_base = current_path.parents[2] / "adventOfCodePrivateFiles"
+
+    input_path = private_files_base / year / day / file_name
+    return input_path
+
+
+def main():
+    pass
+
+if __name__ == "__main__": 
+    pytest.main([__file__])
+    main()
