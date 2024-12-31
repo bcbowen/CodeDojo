1ea1d4c04b05f62c5c3e7e412b77870a25d7251f
bcbowen <bcbowen@gmail.com>
2024-12-31 11:08:54 -0500

    Start day 16

---
+++ adventOfCode/2024/Day16/2024Day16.py
@@ -0,0 +1,17 @@
+import pytest
+from pathlib import Path
+
+
+def part1(file_name: str) -> int: 
+    pass
+
+
+def main(): 
+    pass
+
+def test_part1(): 
+    pass
+
+if __name__ == "__main__": 
+    pytest.main([__file__])
+    main()
