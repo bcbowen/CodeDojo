03f7f6881f1c1f5d2c2a065826e94cea0480841e
Ben Bowen <bcbowen@gmail.com>
2025-02-19 00:10:10 -0500

    Update 104 and helpers

--- leetcode/Easy/1-500/104_MaxDepthBinaryTree.py
+++ leetcode/Easy/1-500/104_MaxDepthBinaryTree.py
@@ -1,17 +1,50 @@
 import pytest
 import sys 
 import os 
+from typing import Optional
 
-helper_path = os.path.abspath(os.path.join(os.path.dirname(__file__), "../..", "Helpers"))
+# Get the absolute path to the root of the project (2 levels up from the current file)
+project_root = os.path.abspath(os.path.join(os.path.dirname(__file__), "../.."))
 
-if helper_path not in sys.path: 
-    sys.path.append(helper_path)
+# Add project root to sys.path if not already there
+if project_root not in sys.path:
+    sys.path.append(project_root)
 
-import BinaryTreeHelpers
+# Now the import should work
+from Helpers.BinaryTreeHelpers import TreeNode, populate_tree, parse_values
+
+class Solution:
+    def maxDepth(self, root: Optional[TreeNode]) -> int:
+        def dive(root : TreeNode, currentDepth: int) -> int: 
+            if not root.left and not root.right: 
+                return currentDepth
+            max_depth = currentDepth
+            if root.left: 
+                max_depth = dive(root.left, currentDepth + 1)
+            if root.right: 
+                max_depth = max(max_depth, dive(root.right, currentDepth + 1))
+            return max_depth
+        if not root: 
+            return 0
+
+        depth = dive(root, 1)
+
+        return depth
+
+
+@pytest.mark.parametrize("definition, expected", [
+    ("[]", 0),
+    ("[3,9,20,null,null,15,7]", 3), 
+    ("[1,null,2]", 2)
+])
+def test_maxDepth(definition: str, expected: int): 
+    root = populate_tree(definition)
+    result = Solution().maxDepth(root)
+    assert(result == expected)
 
 def test_import(): 
     vals = "[1,2,3]"
-    result = BinaryTreeHelpers.parse_values(vals)
+    result = parse_values(vals)
     expected = [1,2,3]
     assert(result == expected)
 
