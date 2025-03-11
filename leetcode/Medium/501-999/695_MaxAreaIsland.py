8aec988e9af25627e46aa24eb5e61844b3eca04e
Ben Bowen <bcbowen@gmail.com>
2025-03-11 00:18:54 -0400

    Start 695

---
+++ leetcode/Medium/501-999/695_MaxAreaIsland.py
@@ -0,0 +1,11 @@
+import pytest
+
+class Solution:
+    def maxAreaOfIsland(self, grid: list[list[int]]) -> int:
+        pass
+
+def test_maxAreaOfIsland(self, grid: list[list[int]], expected: int):
+    pass
+
+if __name__ == "__main__": 
+    pytest.main(__file__)
