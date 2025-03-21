52bf25e11be6cec5e5afc40a21c9a732be1eebee
Ben Bowen <bcbowen@gmail.com>
2025-03-20 22:55:50 -0400

    Finish 55 jump game med

--- leetcode/Medium/1-500/55_JumpGame.py
+++ leetcode/Medium/1-500/55_JumpGame.py
@@ -1,7 +1,11 @@
+import json
 import pytest
+import time 
 from collections import deque
+from pathlib import Path
 
 class Solution:
+    """
     def canJump(self, nums: list[int]) -> bool:
         if len(nums) == 1: 
             return True
@@ -18,7 +22,32 @@ class Solution:
                 if next_pos < len(nums) and not seen[next_pos]: 
                     queue.append(next_pos)
         return False
+    
+        def canJump(self, nums):
+        GOOD, BAD, UNKNOWN = 1, 0, -1
+        memo = [UNKNOWN] * len(nums)
+        memo[-1] = GOOD
+        for i in range(len(nums) - 2, -1, -1):
+            furthest_jump = min(i + nums[i], len(nums) - 1)
+            for j in range(i + 1, furthest_jump + 1):
+                if memo[j] == GOOD:
+                    memo[i] = GOOD
+                    break
+        return memo[0] == GOOD
 
+    """
+    def canJump(self, nums: list[int]) -> bool:
+        GOOD, BAD, UNK = 1, 0, -1
+        memo = [UNK] * len(nums)
+        memo[-1] = GOOD
+        for i in range(len(nums) - 2, -1, -1):
+            furthest_jump = min(i + nums[i], len(nums) - 1)
+            for j in range(i + 1, furthest_jump + 1): 
+                if memo[j] == GOOD: 
+                    memo[i] = GOOD
+                    break
+        return memo[0] == GOOD 
+        
 """
 Example 1:
 Input: nums = [2,3,1,1,4]
@@ -41,6 +70,23 @@ Explanation: You will always arrive at index 3 no matter what. Its maximum jump 
 def test_canJump(nums: list[int], expected: bool):
     result = Solution().canJump(nums)
     assert(result == expected) 
+
+# initial implementation had memory limit exceeded on this case
+def test_biginput77(): 
+    data_path = Path(__file__).parent.parent.parent / "Data"
+    file_name = "55_77MLE.txt"    
+    path = data_path / file_name
+    with open(path, "r") as file: 
+        nums = json.loads(file.readline())
+
+    expected = False
+    start_time = time.perf_counter()  # Start the timer
+    result = Solution().canJump(nums)
+    end_time = time.perf_counter()    # Stop the timer
+
+    execution_time = end_time - start_time
+    print(f"Execution time: {execution_time:.6f} seconds")
+    assert(result == expected)
 
 if __name__ == "__main__":
     pytest.main([__file__]) 
