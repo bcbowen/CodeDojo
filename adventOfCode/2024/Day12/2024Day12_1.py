ffa7059af3abab31b7580d7bf8854684d6969720
Ben Bowen <bbowen@bhg-inc.com>
2025-04-03 11:08:01 -0400

    Breaking 2024 day 12 into 2 parts

---
+++ adventOfCode/2024/Day12/2024Day12_1.py
@@ -0,0 +1,123 @@
+import pytest
+from pathlib import Path
+from collections import namedtuple
+
+Direction = namedtuple('Direction', ['y', 'x'])
+East = Direction(0, 1)
+West = Direction(0, -1)
+South = Direction(1, 0)
+North = Direction(-1, 0)
+
+def get_input_filepath(file_name: str):
+    current_path = Path(__file__).parent
+    day = current_path.name
+    current_path = current_path.parent
+    year = current_path.name
+
+    # traverse up directories to the private files
+    private_files_base = current_path.parents[2] / "adventOfCodePrivateFiles"
+
+    input_path = private_files_base / year / day / file_name
+    return input_path
+
+def load_grid(file_name: str) -> list[list[str]]: 
+    path = get_input_filepath(file_name)
+    with open(path, "r") as file: 
+        grid = [(list(line.strip())) for line in file.readlines()]
+    return grid
+
+"""
+perimeter = + 1 for each side bordered by another value or an edge
+"""
+def calc_perimiter(grid: list[list[str]], row : int, col: int): 
+    val = grid[row][col]
+    p = 0
+    # west
+    if col == 0 or grid[row][col - 1] != val: 
+        p += 1
+    # east
+    if col == len(grid[0]) - 1 or grid[row][col + 1] != val: 
+        p += 1
+    # north
+    if row == 0 or grid[row - 1][col] != val: 
+        p += 1
+    # south 
+    if row == len(grid) - 1 or grid[row + 1][col] != val: 
+        p += 1
+
+    return p
+
+def traverse_connected_cells(grid: list[list[str]], visited: set[tuple[int, int]], position: tuple[int, int], plots : dict[str, list[tuple[int, int]]]): 
+    value = grid[position[0]][position[1]]
+    #visited.add(position)
+    q = []
+    q.append(position)
+    area = 0
+    perimiter = 0
+    cells = []
+    while len(q) > 0: 
+        cell = q.pop(0)
+        if cell in visited: 
+            continue
+
+        visited.add(cell)
+        cells.append(cell)
+        area += 1
+        perimiter += calc_perimiter(grid, cell[0], cell[1])
+
+        # north
+        if cell[0] > 0 and grid[cell[0] - 1][cell[1]] == value: 
+            q.append((cell[0] - 1, cell[1]))
+
+        # east
+        if cell[1] < len(grid) - 1 and grid[cell[0]][cell[1] + 1] == value: 
+            q.append((cell[0], cell[1] + 1))
+        
+        #south
+        if cell[0] < len(grid) - 1 and grid[cell[0] + 1][cell[1]] == value: 
+            q.append((cell[0] + 1, cell[1]))
+
+        #west
+        if cell[1] > 0 and grid[cell[0]][cell[1] - 1] == value: 
+            q.append((cell[0], cell[1] - 1))
+
+    if not value in plots:
+        plots[value] = []
+    plots[value].append((perimiter, area, cells))
+
+def day12_part1(file_name: str) -> int: 
+    grid = load_grid(file_name)
+    # plots key = garden, val = [p, a]
+    plots = {}
+    visited = set()
+    for row in range(len(grid)): 
+        for col in range(len(grid[0])):
+            if ((row, col)) in visited: 
+                continue
+            traverse_connected_cells(grid, visited, (row, col), plots)
+    result = 0
+    for key in plots: 
+        for group in plots[key]: 
+            result += group[0] * group[1]
+
+    return result
+
+def main(): 
+    file_name = "input.txt"
+    result = day12_part1(file_name)
+    print(f"Part 1 result for {file_name}: {result}")
+    
+
+@pytest.mark.parametrize("file_name, expected", [
+    ("sample.txt", 140),
+    ("sample2.txt", 772),
+    ("sample3.txt", 1930) 
+])
+def test_day12_part1(file_name : str, expected: int): 
+    result = day12_part1(file_name)
+    assert(result == expected)
+
+if __name__ == "__main__": 
+    pytest.main([__file__])
+    main()
+
