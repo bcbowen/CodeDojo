17ebe0ffeb6a509d8014f9a5488a5b3439add909
Ben Bowen <bcbowen@gmail.com>
2025-04-23 23:02:09 -0400

    Working on snakes and ladders

--- leetcode/Medium/501-999/909_SnakesAndLadders.py
+++ leetcode/Medium/501-999/909_SnakesAndLadders.py
@@ -1,3 +1,4 @@
+import math
 import pytest
 from typing import List
 
@@ -16,14 +17,29 @@ class Solution:
     01 02 03 04
     """
     def get_coordinates(board: List[List[int]], space: int) -> tuple[int, int]:
-        rows = space // len(board[0])
-        cols = space % len(board[0])
-        row = len(board) - rows - 1
-        if row % 2 == 1:
-            col = cols - 1
-        else:
-            col = len(board[0]) - cols - 1
+        # The board is square so rows == cols
+        side = len(board[0])
+        
+        row = Solution.get_row(side, space)
+        col = Solution.get_col(side, space)
         return (row, col)
+    
+    def get_col(row_size: int, space: int) -> int: 
+        if space == 1: 
+            return 0
+        
+        row = Solution.get_row(row_size, space)
+        if row % 2 == 1:
+            col = (space - 1) % row_size
+        else:
+            col = row_size - (space % row_size) 
+        return col
+
+
+    def get_row(row_size: int, space: int) -> int:
+        row = row_size - math.ceil(space / row_size)
+        return row 
+
 """
 Example 1:
 Input: board = [
@@ -117,6 +133,47 @@ def test_snakesAndLadders(board: List[List[int]], expected: int):
 def test_get_coordinates(board : List[List[int]], space: int, expected: tuple[int, int]):
     result = Solution.get_coordinates(board, space)
     assert(result == expected)
+
+"""
+    16 15 14 13
+    09 10 11 12
+    08 07 06 05
+    01 02 03 04
+"""
+@pytest.mark.parametrize("row_size, space, expected", [
+    (4, 1, 0),
+    (4, 2, 1),
+    (4, 3, 2),
+    (4, 4, 3),
+    (4, 8, 0),
+    (4, 10, 1),
+    (4, 14, 2),
+    (4, 12, 3), 
+])
+def test_get_col(row_size: int, space: int, expected: int): 
+    result = Solution.get_col(row_size, space)
+    assert(result == expected)
+
+"""
+    16 15 14 13
+    09 10 11 12
+    08 07 06 05
+    01 02 03 04
+"""
+@pytest.mark.parametrize("row_size, space, expected", [
+    (4, 1, 3),
+    (4, 8, 2),
+    (4, 9, 1),
+    (4, 16, 0),
+    (4, 2, 3),
+    (4, 6, 2),
+    (4, 12, 1),
+    (4, 14, 0), 
+])
+def test_get_row(row_size: int, space: int, expected: int): 
+    result = Solution.get_row(row_size, space)
+    assert(result == expected)
+
 
 if __name__ == "__main__":
     pytest.main([__file__])
