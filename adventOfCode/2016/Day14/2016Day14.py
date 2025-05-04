81999c0c5da0c4b3e6b4a0fb80b170b43e36fefe
Ben Bowen <bcbowen@gmail.com>
2025-05-04 17:23:07 -0400

    Finish 2016 day14, work on 909 snakes and ladders

--- adventOfCode/2016/Day14/2016Day14.py
+++ adventOfCode/2016/Day14/2016Day14.py
@@ -1,76 +1,96 @@
+from functools import lru_cache
 import hashlib
 import pytest
 import re
 
 def main(): 
-    print(hashlib.md5(b'abc18').hexdigest())
+    salt = "cuanljph"
+    result = day14(salt, 1)
+    print(f"Part1: {result}")
+    result = day14(salt, 2017)
+    print(f"Part2: {result}")
 
-def part1(salt: str) -> int: 
-    candidates = {} 
+@lru_cache(maxsize=1000)
+def get_digest(salt: str, val: int, rounds: int) -> str: 
+    s = (salt + str(val))
+    for _ in range(rounds): 
+        s = hashlib.md5(s.encode()).hexdigest()
+    return s
+
+def day14(salt: str, rounds: int) -> int: 
+    confirmations = {} 
+
     keys = {}
+    def add_found_key(key: str, index: int): 
+        nonlocal key_count
+        if not key in keys: 
+            keys[key] = [] 
+        keys[key].append(index)
+        key_count += 1
+        print(f'{key}({key_count}) {index}')
+
     key_count = 0
     i = 0
     while key_count < 64: 
-        #if i > 900: 
-        #    break
+        hashed = get_digest(salt, i, rounds)
+        pattern3 = r"(\w)\1{2,}"
+        pattern5 = r"(\w)\1{4,}"
 
-        val = (salt + str(i)).encode()
-        hashed = hashlib.md5(val).hexdigest()
-        pattern = r'(\w)\1{2,}'
-
-        """
-        def get_first_repeat(s):
-    # Find the first sequence of at least 3 repeated characters
-    match = re.search(r'(\w)\1{2,}', s)
-    if match:
-        repeated_seq = match.group()
-        if len(repeated_seq) >= 5:
-            return repeated_seq
-        else:
-            return repeated_seq[:3]
-    return None
-        """
-        match = re.search(pattern, hashed)
+        match = re.search(pattern3, hashed)
         if match:
             match_group = match.group() 
-            if len(match_group) >= 5: 
-                if match_group[0] in candidates and i - candidates[match_group[0]] < 1000:
-                    keys[match_group] = i
-                    del candidates[match_group[0]] 
-                    key_count += 1
+            key = match_group[0]
+            found = False
+            if key in confirmations and confirmations[key] > i: 
+                add_found_key(key, confirmations[key]) 
             else: 
-                if not match_group[0] in candidates: 
-                    candidates[match_group[0]] = 0
-                candidates[match_group[0]] = i
-            
-        i += 1
+                for j in range(i + 1, i + 1001): 
+                    hashed = get_digest(salt, j, rounds)
+                    match = re.search(pattern5, hashed)
+                    if match: 
+                        match_group = match.group()
+                        if match_group[0] == key: 
+                            found = True
+                        confirm_key = match_group[0]
 
-    return i
+                        if not confirm_key in confirmations: 
+                            confirmations[confirm_key] = 0
+                        confirmations[confirm_key] = j
+                        if found: 
+                            add_found_key(confirm_key, confirmations[confirm_key]) 
+                            break
+        i += 1
+        
+       
+
+    return i - 1
                         
     
 def test_part1(): 
     salt = 'abc'
     expected = 22728
-    result = part1(salt)
+    result = day14(salt, 1)
     assert(expected == result)
 
-def test_capture3(): 
-    s = "0034e0923cc38887aaaab57bd7b1d4f953dfbbbbba"
+def test_part2(): 
+    salt = 'abc'
+    expected = 22859
+    result = day14(salt, 2017)
+    assert(expected == result)
 
-    pattern = r'(\w)\1{2,}'
-    match = re.search(pattern, s)
+
+@pytest.mark.parametrize("hash, pattern, expected", [
+    ("12aaa34567890", r"(\w)\1{2,}", "aaa"), 
+    ("12aaaaa34567890", r"(\w)\1{4,}", "aaaaa"), 
+    ("12aaa3456bbb7890", r"(\w)\1{2,}", "aaa"), 
+    ("12aaa3456bbbbb7890", r"(\w)\1{2,}", "aaa"), 
+    ("12aaaaa34567890", r"(\w)\1{4,}", "aaaaa"), 
+    ("12aaa345bbbbb67890", r"(\w)\1{4,}", "bbbbb")
+])
+def test_capture(hash: str, pattern: str, expected: str):   
+    match = re.search(pattern, hash)
     assert(match) 
     match_group = match.group()
-    expected = '888'
-    assert(match_group == expected)
-
-def test_capture5(): 
-    s = "0034e0923cc38887a57bd7b1d4f953dfaaaabbbbbccccc888"
-    pattern = r'(\w)\1{4,}'
-    match = re.search(pattern, s)
-    assert(match) 
-    match_group = match.group()
-    expected = 'bbbbb'
     assert(match_group == expected)
 
 if __name__ == "__main__":
