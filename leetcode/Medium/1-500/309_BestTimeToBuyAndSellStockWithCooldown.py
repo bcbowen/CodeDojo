18fc7c764e8a51977a16cdc35c1d35fb06d787cb
bcbowen <bcbowen@gmail.com>
2025-05-14 13:29:37 -0400

    309 py in progress

---
+++ leetcode/Medium/1-500/309_BestTimeToBuyAndSellStockWithCooldown.py
@@ -0,0 +1,28 @@
+import pytest
+from typing import List
+
+class Solution:
+    def maxProfit(self, prices: List[int]) -> int:
+        pass
+
+"""
+Example 1:
+Input: prices = [1,2,3,0,2]
+Output: 3
+Explanation: transactions = [buy, sell, cooldown, buy, sell]
+
+Example 2:
+ Input: prices = [1]
+Output: 0
+"""
+@pytest.mark.parametrize("prices, expected", [
+    ([1,2,3,0,2], 3), 
+    ([1], 0)
+])
+def test_maxProfit(prices: List[int], expected: int):
+    result = Solution().maxProfit(prices)
+    assert(result == expected)
+
+
+if __name__ == "__main__": 
+    pytest.main([__file__])
