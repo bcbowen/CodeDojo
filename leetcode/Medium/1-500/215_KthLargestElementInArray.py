5071b9ba8c795d72a90475f1cded9f84bfbcd2a5
Ben Bowen <bcbowen@gmail.com>
2025-05-22 21:19:49 -0400

    Fix pylint warning

--- leetcode/Medium/1-500/215_KthLargestElementInArray.py
+++ leetcode/Medium/1-500/215_KthLargestElementInArray.py
@@ -7,6 +7,7 @@ class Solution:
     def findKthLargest(self, nums: List[int], k: int) -> int:
         max_heap = [-num for num in nums] 
         heapq.heapify(max_heap)
+        val = -1
         for _ in range(k): 
             val = heapq.heappop(max_heap)
         return -val
