8e86a84cd37f65e0e69fb9bc8371c71a4e47f6b5
bcbowen <bcbowen@gmail.com>
2025-06-11 11:31:53 -0400

    Finish detonate bombs

--- leetcode/Medium/1000-5000/2101_DetonateMaxBombs.py
+++ leetcode/Medium/1000-5000/2101_DetonateMaxBombs.py
@@ -5,11 +5,50 @@ from typing import List
 
 class Solution:
     def maximumDetonation(self, bombs: List[List[int]]) -> int:
-        pass
+        # bomb 1 blast reached bomb 2 (center of bomb2 circle)
+        def reaches_bomb(x1, y1, x2, y2, radius):
+            distance = math.sqrt((x2 - x1)**2 + (y2 - y1)**2)
+            return distance <= radius
 
-def circles_overlap(x1, y1, r1, x2, y2, r2):
-    distance = math.sqrt((x2 - x1)**2 + (y2 - y1)**2)
-    return distance < (r1 + r2)
+        bomb_graph = {} 
+        
+        for i in range(len(bombs)): 
+            if not i in bomb_graph: 
+                        bomb_graph[i] = []
+            for j in range(i + 1, len(bombs)): 
+                xi, yi, ri = bombs[i]
+                xj, yj, rj = bombs[j]
+                # does bomb i blast reach bomb j? 
+                if reaches_bomb(xi, yi, xj, yj, ri): 
+                    bomb_graph[i].append(j)
+                # does bomb j blast reach bomb i? 
+                if reaches_bomb(xi, yi, xj, yj, rj): 
+                    if not j in bomb_graph: 
+                        bomb_graph[j] = [] 
+                    bomb_graph[j].append(i)
+
+        bomb_stack = [] 
+        max_detonations = 0
+
+        for bomb in bomb_graph: 
+            detonations = 0
+            bomb_stack.clear()
+            bomb_stack.append(bomb)
+            seen = set()
+            seen.add(bomb)
+            while bomb_stack: 
+                current = bomb_stack.pop()
+                detonations += 1
+                for next in bomb_graph[current]: 
+                    if not next in seen: 
+                        seen.add(next) 
+                        bomb_stack.append(next)
+            max_detonations = max(max_detonations, detonations)
+
+        return max_detonations
+            
+
+
 
 """
 Example 1:
@@ -36,12 +75,22 @@ The best bomb to detonate is bomb 0 because:
 - Bomb 2 detonates bomb 3. The blue circle denotes the range of bomb 2.
 - Bomb 3 detonates bomb 4. The green circle denotes the range of bomb 3.
 Thus all 5 bombs are detonated.
+
+
+TC 62: 
+bombs: [[54,95,4],[99,46,3],[29,21,3],[96,72,8],[49,43,3],[11,20,3],[2,57,1],[69,51,7],[97,1,10],[85,45,2],[38,47,1],[83,75,3],[65,59,3],[33,4,1],[32,10,2],[20,97,8],[35,37,3]]
+expected: 1
+
 """
-@pytest.mark.parametrize("", [
-     
+@pytest.mark.parametrize("bombs, expected", [
+     ([[2,1,3],[6,1,4]], 2), 
+     ([[1,1,5],[10,10,5]], 1), 
+     ([[1,2,3],[2,3,1],[3,4,2],[4,5,3],[5,6,4]], 5), 
+     ([[54,95,4],[99,46,3],[29,21,3],[96,72,8],[49,43,3],[11,20,3],[2,57,1],[69,51,7],[97,1,10],[85,45,2],[38,47,1],[83,75,3],[65,59,3],[33,4,1],[32,10,2],[20,97,8],[35,37,3]], 1)
 ])
 def test_maximumDetonation(bombs: List[List[int]], expected: int):
-        pass
+    result = Solution().maximumDetonation(bombs)
+    assert(result == expected)
 
 if __name__ == "__main__":
     pytest.main([__file__]) 
