aa7355c1e9f5e7c5dcc144ad61f3019a9367560d
bcbowen <bcbowen@gmail.com>
2025-07-07 11:23:33 -0400

    Cheapest Flights withing k stops initial setup

---
+++ leetcode/Medium/501-999/787_CheapestFlightsWithinKStops.py
@@ -0,0 +1,42 @@
+import pytest
+from typing import List
+
+class Solution:
+    def findCheapestPrice(self, n: int, flights: List[List[int]], src: int, dst: int, k: int) -> int:
+        return 2
+       
+"""
+Example 1:
+Input: n = 4, flights = [[0,1,100],[1,2,100],[2,0,100],[1,3,600],[2,3,200]], src = 0, dst = 3, k = 1
+Output: 700
+Explanation:
+The graph is shown above.
+The optimal path with at most 1 stop from city 0 to 3 is marked in red and has cost 100 + 600 = 700.
+Note that the path through cities [0,1,2,3] is cheaper but is invalid because it uses 2 stops.
+
+Example 2:
+Input: n = 3, flights = [[0,1,100],[1,2,100],[0,2,500]], src = 0, dst = 2, k = 1
+Output: 200
+Explanation:
+The graph is shown above.
+The optimal path with at most 1 stop from city 0 to 2 is marked in red and has cost 100 + 100 = 200.
+
+Example 3:
+Input: n = 3, flights = [[0,1,100],[1,2,100],[0,2,500]], src = 0, dst = 2, k = 0
+Output: 500
+Explanation:
+The graph is shown above.
+The optimal path with no stops from city 0 to 2 is marked in red and has cost 500.
+ 
+"""
+@pytest.mark.parametrize("n, flights, src, dst, k, expected", [
+    (4, [[0,1,100],[1,2,100],[2,0,100],[1,3,600],[2,3,200]], 0, 3, 1, 700), 
+    (3, [[0,1,100],[1,2,100],[0,2,500]], 0, 2, 1, 200), 
+    (3, [[0,1,100],[1,2,100],[0,2,500]], 0, 2, 0, 500)
+])
+def test_findCheapestPrice(n: int, flights: List[List[int]], src: int, dst: int, k: int, expected: int):
+    result = Solution().findCheapestPrice(n, flights, src, dst, k)
+    assert(result == expected)
+
+if __name__ == "__main__": 
+    pytest.main([__file__])
