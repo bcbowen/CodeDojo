ceabce79ae5fe8856bf1902537bf17ff7993a5bc
Ben Bowen <bcbowen@gmail.com>
2025-11-16 22:16:53 -0500

    1437 K Length apart

---
+++ leetcode/Easy/1001-5000/1437_All1sAreKSpacesAway.py
@@ -0,0 +1,13 @@
+from typing import List 
+
+class Solution:
+    def kLengthApart(self, nums: List[int], k: int) -> bool:
+        dist = k
+        for n in nums: 
+            if n == 1: 
+                if dist < k: 
+                    return False
+                dist = 0
+            else: 
+                dist += 1
+        return True
