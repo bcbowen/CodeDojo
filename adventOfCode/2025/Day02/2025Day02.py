46f9565911aea257831be0500863844042eaf536
Ben Bowen <bcbowen@gmail.com>
2025-12-02 22:51:06 -0500

    Working on day 2... not quite there

--- adventOfCode/2025/Day02/2025Day02.py
+++ adventOfCode/2025/Day02/2025Day02.py
@@ -14,8 +14,33 @@ def get_input_filepath(file_name: str) -> Path:
     input_path = private_files_base / year / day / file_name
     return input_path
 
+def load_inputs(file_name: str) -> List[Tuple[int, int]]: 
+    path = get_input_filepath(file_name)
+    result = [] 
+    with open(path, "r") as file: 
+        line = file.readline()
+    inputs = line.split(',')
+    for input in inputs:
+        vals = input.split('-') 
+        result.append((int(vals[0]), int(vals[1])))
+
+
+    return result 
+
+def part1(file_name: str) -> int: 
+    inputs = load_inputs(file_name)
+    result = 0
+    for start, end in inputs:
+        ids = find_invalid_ids(start, end)
+        result += sum(ids)
+    return result 
+
 def main(): 
-    pass
+    file_name = "input.txt"
+    result = part1(file_name)
+    print(f"Part 1 result: {result}")
+
+    # too low: 33906626186
 
 def get_max_value(current: int) -> int: 
     val_text = str(current)
@@ -52,6 +77,15 @@ def find_valid_ranges(start: int, end: int) -> List[Tuple[int, int]]:
     Returns the number of invalid ids in this range
 """
 def find_invalid_ids_in_range(start: int, end: int) -> List[int]: 
+    def increment_current(): 
+        nonlocal current
+        c_val = int(''.join([str(d) for d in current[0:mid_count]]))
+        c_val += 1
+        c_text = str(c_val)
+        #for i in range(mid_count): 
+        #    current[i] = int(c_text[i])
+        current = [int(d) for d in c_text] + [0] * mid_count 
+
     result = []
     start_digits = [int(d) for d in str(start)]
     end_digits = [int(d) for d in str(end)]
@@ -62,13 +96,14 @@ def find_invalid_ids_in_range(start: int, end: int) -> List[int]:
 
     current = start_digits.copy()
 
-    while current != end_digits: 
-        val = int(''.join([str(d) for d in current]))
-        if val <= end: 
+    while len(current) == len(end_digits) and current <= end_digits: 
+        val = int(''.join([str(d) for d in current[0:mid_count]]) * 2)
+        if val >= start and val <= end: 
             result.append(val)
+            increment_current()
         
-        
-    #start_left_text = str(start[0:len()]
+        #else: 
+        #    break
 
     return result
 
@@ -82,8 +117,8 @@ of digits
 def find_invalid_ids(start: int, end: int) -> List[int]: 
     result = [] 
     ranges = find_valid_ranges(start, end)
-    for valid_rangge in ranges: 
-
+    for range_start, range_end in ranges: 
+        result.extend(find_invalid_ids_in_range(range_start, range_end))
     return result
 
 
@@ -95,7 +130,7 @@ def find_invalid_ids(start: int, end: int) -> List[int]:
     (222220, 222224, [222222]), 
     (1698522, 1698528, []), 
     (446443, 446449, [446446]), 
-    (38593856, 38593862, 38593859), 
+    (38593856, 38593862, [38593859]), 
     (565653, 565659, []), 
     (824824821, 824824827, []), 
     (2121212118, 2121212124, [])
@@ -139,10 +174,23 @@ def test_max_value(val: int, expected: int):
 @pytest.mark.parametrize("start, end, expected", [
     (11, 22, [11, 22]), 
     (11, 35, [11, 22, 33]), 
-    (9_595_822_750, 9_596_086_139, [9_595_895_958, 9_595_995_959])
+    (9_595_822_750, 9_596_086_139, [9_595_895_958, 9_595_995_959]), 
+    (1957, 2424, [2020, 2121, 2222, 2323, 2424])
 ])
 def test_find_invalid_ids_in_range(start: int, end: int, expected: List[int]): 
     result = find_invalid_ids_in_range(start, end)
+    assert(result == expected)
+
+def test_load_inputs(): 
+    file_name = "sample.txt"
+    inputs = load_inputs(file_name)
+    assert(len(inputs) > 0)
+    assert(inputs[0] == (11, 22))
+
+def test_part1(): 
+    expected = 1227775554
+    file_name = "sample.txt"
+    result = part1(file_name)
     assert(result == expected)
 
 if __name__ == "__main__":
