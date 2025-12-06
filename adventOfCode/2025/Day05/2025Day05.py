0092bc73528c72bb3f426b3254bc712c9b3976c0
Ben Bowen <bcbowen@gmail.com>
2025-12-06 09:25:21 -0500

    Day5 part1 done

--- adventOfCode/2025/Day05/2025Day05.py
+++ adventOfCode/2025/Day05/2025Day05.py
@@ -26,6 +26,40 @@ class ProductRange:
           product = products[i]
           return (i, product_id >= product.min_id and product_id <= product.max_id)
 
+    @staticmethod
+    def parse(line: str) -> "ProductRange": 
+         fields = line.split('-')
+         return ProductRange(int(fields[0]), int(fields[1]))
+
+    """
+        23-40
+        35-60
+        23-25
+        40-60
+
+        41-50
+
+    """
+    @staticmethod
+    def moige(product_ranges: List[ProductRange]) -> List["ProductRange"]: 
+        product_ranges.sort(key = lambda p: p.max_id)
+
+        current_range : "ProductRange" = product_ranges[0]
+        moiged = []
+        for i in range(1, len(product_ranges)): 
+            if product_ranges[i].min_id > current_range.max_id: 
+                moiged.append(current_range)
+                current_range = product_ranges[i]
+            else: 
+                current_range.max_id = max(product_ranges[i].max_id, current_range.max_id)
+        
+        if current_range.min_id > moiged[-1].max_id: 
+            moiged.append(current_range)
+
+        return moiged
+
+
+
 def get_input_filepath(file_name: str) -> Path:
         current_path = Path(__file__).parent
         day = current_path.name
@@ -38,8 +72,88 @@ def get_input_filepath(file_name: str) -> Path:
         input_path = private_files_base / year / day / file_name
         return input_path
 
+def get_inputs(file_name: str) -> Tuple[List[ProductRange], List[int]]: 
+    path = get_input_filepath(file_name)
+    product_ranges = [] 
+    product_ids = []
+    with open(path, "r") as file: 
+        for line in file.readlines():
+            if line.strip() == '': 
+                 continue
+            elif '-' in line: 
+                product_ranges.append(ProductRange.parse(line))
+            else: 
+                product_ids.append(int(line))
+    
+    #product_ranges = ProductRange.moige(product_ranges)
+
+    return (product_ranges, product_ids) 
+
+
+def part1(file_name: str) -> int: 
+    product_ranges, product_ids = get_inputs(file_name)
+    fresh_count = 0
+    for id in product_ids: 
+        _, is_good = ProductRange.find(product_ranges, id)
+        if is_good:    
+            fresh_count += 1
+    return fresh_count
+ 
+def part1_brute(file_name: str) -> int:  
+    product_ranges, product_ids = get_inputs(file_name)
+    fresh_count = 0
+    for id in product_ids: 
+        for pr in product_ranges: 
+            if pr.min_id <= id <= pr.max_id: 
+                fresh_count += 1
+                #print(f"fresh product: {id}")
+                break
+    return fresh_count
+
+def part2(file_name: str) -> int: 
+    product_ranges, _ = get_inputs(file_name)
+    #fresh_ids = set()
+    fresh_count = 0
+    product_ranges = ProductRange.moige(product_ranges)
+    for pr in product_ranges: 
+        #for p in range(pr.min_id, pr.max_id + 1): 
+        #    fresh_ids.add(p)
+        fresh_count += pr.max_id - pr.min_id + 1
+    return fresh_count
+
+
 def main(): 
-    pass
+    file_name = "input.txt"
+    result = part1_brute(file_name)
+    print(f"Part 1 result: {result}")
+
+    # 336047116961610 too low
+    result = part2(file_name)
+    print(f"Part 2 result: {result}")
+
+def test_part1(): 
+    file_name = "sample.txt"
+    expected = 3
+    result = part1(file_name)
+    assert(result == expected)
+
+def test_part1_brute(): 
+    file_name = "sample.txt"
+    expected = 3
+    result = part1_brute(file_name)
+    assert(result == expected)
+
+def test_part2(): 
+    file_name = "sample.txt"
+    expected = 14
+    result = part2(file_name)
+    assert(result == expected)
+
+def test_load_inputs(): 
+    file_name = "sample.txt"
+    products, product_ids = get_inputs(file_name)
+    assert(len(products) > 1)
+    assert(len(product_ids) == 6)
 
 """
 100 product ranges [1 - 5, 11 - 15, ..., 991 - 995]
