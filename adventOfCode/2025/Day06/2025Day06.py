df6e810b1f00f5c5f6f789101871eed95f0a42c6
Ben Bowen <bcbowen@gmail.com>
2025-12-07 09:23:57 -0500

    Day 6 part 1

--- adventOfCode/2025/Day06/2025Day06.py
+++ adventOfCode/2025/Day06/2025Day06.py
@@ -1,24 +1,112 @@
+import math
 import pytest
-#from typing import List
+from typing import List
 from pathlib import Path
 
+class HomeworkProblem: 
+
+    def __init__(self): 
+        self.values = [] 
+        self.operator = '_'
+
+    def calculate(self) -> int: 
+        match self.operator: 
+            case '+': 
+                return sum(self.values)
+            case '*': 
+                return math.prod(self.values)
+            case _: 
+                return 0
+        
+    @staticmethod
+    def calculate_all(problems: List["HomeworkProblem"]) -> int: 
+        total = 0
+        for p in problems: 
+            total += p.calculate()
+
+        return total
+
 def get_input_filepath(file_name: str) -> Path:
-        current_path = Path(__file__).parent
-        day = current_path.name
-        current_path = current_path.parent
-        year = current_path.name
+    current_path = Path(__file__).parent
+    day = current_path.name
+    current_path = current_path.parent
+    year = current_path.name
 
-        # traverse up directories to the private files
-        private_files_base = current_path.parents[2] / "adventOfCodePrivateFiles"
+    # traverse up directories to the private files
+    private_files_base = current_path.parents[2] / "adventOfCodePrivateFiles"
 
-        input_path = private_files_base / year / day / file_name
-        return input_path
+    input_path = private_files_base / year / day / file_name
+    return input_path
+
+def get_inputs(file_name: str) -> List[HomeworkProblem]: 
+    inputs = [] 
+    lines = []
+    path = get_input_filepath(file_name)
+    with open(path, "r") as file: 
+        lines = file.readlines()
+
+    # get numerical values
+    for i in range(len(lines) - 1):
+        values = lines[i].split()
+        if len(inputs) == 0: 
+            for _ in range(len(values)): 
+                inputs.append(HomeworkProblem())
+        
+        for i in range(len(values)): 
+            inputs[i].values.append(int(values[i]))
+
+    # get operators
+    i = len(lines) - 1
+    values = lines[i].split()
+    for i in range(len(values)): 
+        inputs[i].operator = values[i]
+    return inputs
 
 def main(): 
-    pass
+    file_name = 'input.txt'
+    result = part1(file_name)
+    print(f"Part 1 result: {result}")
 
-def get_next_value(line: str, index: int) -> str:
-    return ""
+def part1(file_name: str) -> int: 
+    inputs = get_inputs(file_name)
+    return HomeworkProblem.calculate_all(inputs)
+
+def test_part1(): 
+    file_name = "sample.txt"
+    expected = 4277556
+    result = part1(file_name)
+    assert(result == expected)
+
+
+@pytest.mark.parametrize("values, operator, expected", [
+    ([123, 45, 6], '*', 33210), 
+    ([328, 64, 98], '+', 490), 
+    ([51, 387, 215], '*', 4243455), 
+    ([64, 23, 314], '+', 401)
+])
+def test_calculate(values: List[int], operator: str, expected: int): 
+    p = HomeworkProblem() 
+    p.values.extend(values)
+    p.operator = operator
+    result = p.calculate()
+    assert(result == expected)
+
+
+def test_get_inputs(): 
+    file_name = "sample.txt"
+    inputs = get_inputs(file_name)
+    assert(len(inputs) == 4)
+    p = inputs[0]
+    assert(len(p.values) == 3)
+    assert(p.operator == '*')
+    assert(p.values[0] == 123)
+    assert(p.values[2] == 6)
+
+    p = inputs[3]
+    assert(len(p.values) == 3)
+    assert(p.operator == '+')
+    assert(p.values[0] == 64)
+    assert(p.values[2] == 314)
 
 if __name__ == "__main__":
     pytest.main([__file__])
