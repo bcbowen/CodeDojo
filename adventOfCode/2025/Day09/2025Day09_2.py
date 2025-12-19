7d75d88e7e92917eea115efbb416baa0d83e93b0
Ben Bowen <bcbowen@gmail.com>
2025-12-19 00:17:00 -0500

    Day 9 part 2

---
+++ adventOfCode/2025/Day09/2025Day09_2.py
@@ -0,0 +1,96 @@
+import pytest
+from collections import deque
+from typing import List, Tuple
+from pathlib import Path
+
+def get_input_filepath(file_name: str) -> Path:
+    current_path = Path(__file__).parent
+    day = current_path.name
+    current_path = current_path.parent
+    year = current_path.name
+
+    # traverse up directories to the private files
+    private_files_base = current_path.parents[2] / "adventOfCodePrivateFiles"
+
+    input_path = private_files_base / year / day / file_name
+    return input_path
+
+def get_inputs(file_name: str) -> List[Tuple[int, int]]:
+    path = get_input_filepath(file_name)
+    inputs = []
+    with open(path, "r") as file: 
+        for line in file:
+            a, b = map(int, line.strip().split(','))
+            inputs.append((a, b))
+
+    return inputs
+
+
+def part2(file_name: str) -> int: 
+
+    def valid(x1, y1, x2, y2):
+        cx1, cx2 = sorted([xs.index(x1) * 2, xs.index(x2) * 2])
+        cy1, cy2 = sorted([ys.index(y1) * 2, ys.index(y2) * 2])
+        left = psa[cx1 - 1][cy2] if cx1 > 0 else 0
+        top = psa[cx2][cy1 - 1] if cy1 > 0 else 0
+        topleft = psa[cx1 - 1][cy1 - 1] if cx1 > 0 < cy1 else 0
+        count = psa[cx2][cy2] - left - top + topleft
+        return count == (cx2 - cx1 + 1) * (cy2 - cy1 + 1)
+
+    points = get_inputs(file_name)
+
+    xs = sorted({ x for x, _ in points })
+    ys = sorted({ y for _, y in points })
+
+    grid = [[0] * (len(ys) * 2 - 1) for _ in range(len(xs) * 2 - 1)]
+
+    for (x1, y1), (x2, y2) in zip(points, points[1:] + points[:1]):
+        cx1, cx2 = sorted([xs.index(x1) * 2, xs.index(x2) * 2])
+        cy1, cy2 = sorted([ys.index(y1) * 2, ys.index(y2) * 2])
+        for cx in range(cx1, cx2 + 1):
+            for cy in range(cy1, cy2 + 1):
+                grid[cx][cy] = 1
+
+    outside = {(-1, -1)}
+    queue = deque(outside)
+
+    while len(queue) > 0:
+        tx, ty = queue.popleft()
+        for nx, ny in [(tx - 1, ty), (tx + 1, ty), (tx, ty - 1), (tx, ty + 1)]:
+            if nx < -1 or ny < -1 or nx > len(grid) or ny > len(grid[0]): continue
+            if 0 <= nx < len(grid) and 0 <= ny < len(grid[0]) and grid[nx][ny] == 1: continue
+            if (nx, ny) in outside: continue
+            outside.add((nx, ny))
+            queue.append((nx, ny))
+
+    for x in range(len(grid)):
+        for y in range(len(grid[0])):
+            if (x, y) not in outside:
+                grid[x][y] = 1
+
+    psa = [[0] * len(row) for row in grid]
+
+    for x in range(len(psa)):
+        for y in range(len(psa[0])):
+            left = psa[x - 1][y] if x > 0 else 0
+            top = psa[x][y - 1] if y > 0 else 0
+            topleft = psa[x - 1][y - 1] if x > 0 < y else 0
+            psa[x][y] = left + top - topleft + grid[x][y]
+
+    return max((abs(x1 - x2) + 1) * (abs(y1 - y2) + 1) for i, (x1, y1) in enumerate(points) for x2, y2 in points[:i] if valid(x1, y1, x2, y2))
+
+
+def main(): 
+    file_name = "input.txt"
+    result = part2(file_name)
+    print(f"Part2 result: {result}")
+
+def test_part2(): 
+    file_name = "sample.txt"
+    expected = 24
+    result = part2(file_name)
+    assert(result == expected)
+
+if __name__ == "__main__":
+    pytest.main([__file__])
+    main()
