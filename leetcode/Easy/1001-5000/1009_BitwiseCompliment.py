59c60959046a71e2d3d8a1570b33890c8ee5f094
Ben Bowen <bcbowen@gmail.com>
2026-01-06 22:40:51 -0500

    1009 bitwise compliment

---
+++ leetcode/Easy/1001-5000/1009_BitwiseCompliment.py
@@ -0,0 +1,44 @@
+
+import pytest
+
+
+class Solution:
+    def bitwiseComplement(self, n: int) -> int:
+        result = 0
+        
+        next = 1
+        while n > 0: 
+            if (n & 1) == 0: 
+                result |= next
+            next *= 2
+            n >>= 1
+        return result
+    
+
+"""
+Example 1:
+Input: n = 5
+Output: 2
+Explanation: 5 is "101" in binary, with complement "010" in binary, which is 2 in base-10.
+
+Example 2:
+Input: n = 7
+Output: 0
+Explanation: 7 is "111" in binary, with complement "000" in binary, which is 0 in base-10.
+
+Example 3:
+Input: n = 10
+Output: 5
+Explanation: 10 is "1010" in binary, with complement "0101" in binary, which is 5 in base-10.
+"""
+@pytest.mark.parametrize("n, expected", [
+    (5, 2),
+    (7, 0),
+    (10, 5)
+])
+def test_bitwiseComplement(n: int, expected: int):
+    result = Solution().bitwiseComplement(n)
+    assert(result == expected)
+
+if __name__ == "__main__":
+    pytest.main([__file__]) 
