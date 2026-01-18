5e04897e80f7e15a52ecdcc8b48f0cda902d7638
Ben Bowen <bcbowen@gmail.com>
2026-01-17 22:57:03 -0500

    3740 Min Dist Between 3 equal items

--- leetcode/Easy/1001-5000/3740_MinDistBetween3EqualItems.py
+++ leetcode/Easy/1001-5000/3740_MinDistBetween3EqualItems.py
@@ -1,19 +1,38 @@
 import pytest
 
-from collections import defaultdict
-from typing import List
+#from collections import defaultdict
+from typing import List, Tuple
 
 
 
 class Solution:
     def minimumDistance(self, nums: List[int]) -> int:
-        counts = defaultdict(int)
-        for i in range(len(nums)): 
-            counts[nums[i]] += 1
+        counts = {}
+        min_dist = 1001
+        for i in range(len(nums)):
+            if not nums[i] in counts: 
+                counts[nums[i]] = [] 
+            counts[nums[i]].append(i)
+        for key, indices in counts.items(): 
+            if len(indices) > 2: 
+                dist = self.find_min_dist(indices)
+                min_dist = min(dist, min_dist) 
+        if min_dist == 1001: 
+            min_dist = -1
+        return min_dist
     
-    def find_min_dist(self, indexes : List[int]) -> int: 
-        min_dist = float('inf')
+    def find_dist(self, values: Tuple[int, int, int]) -> int:
+        # abs(i - j) + abs(j - k) + abs(k - i)
+        i, j, k = values
+        return abs(i - j) + abs(j - k) + abs(k - i)
 
+    def find_min_dist(self, indices : List[int]) -> int: 
+        min_dist = 1001
+        triplets = self.find_triplets(indices)
+        for i, j, k in triplets: 
+            dist = self.find_dist((i, j, k))
+            min_dist = min(dist, min_dist)
+        return min_dist
     
     def find_triplets(self, values: List[int]) -> List[List[int]]: 
         if len(values) < 3: 
@@ -34,6 +53,28 @@ def test_find_triplets(values: List[int], expected: List[List[int]]):
     result = Solution().find_triplets(values)
     assert(result == expected)
 
+"""
+The minimum distance is achieved by the good tuple (0, 2, 3).
+
+(0, 2, 3) is a good tuple because nums[0] == nums[2] == nums[3] == 1. Its distance is abs(0 - 2) + abs(2 - 3) + abs(3 - 0) = 2 + 1 + 3 = 6.
+
+Example 2:
+
+Input: nums = [1,1,2,3,2,1,2]
+
+Output: 8
+
+Explanation:
+
+The minimum distance is achieved by the good tuple (2, 4, 6).
+"""
+@pytest.mark.parametrize("", [
+    (0, 2, 3, 6), 
+    (2, 4, 6, 8)
+])
+def find_dist_test(i: int, j: int, k: int, expected: int):
+    result = Solution().find_dist((i, j, k))
+    assert(result == expected) 
 
 
 """
@@ -56,11 +97,17 @@ Input: nums = [1]
 Output: -1
 Explanation:
 There are no good tuples. Therefore, the answer is -1.
+
+TC 826
+[1,2,1,1,2,2] Expected: 6
+
+
 """
 @pytest.mark.parametrize("nums, expected", [
     ([1,2,1,1,3], 6),
     ([1,1,2,3,2,1,2], 8), 
-    ([1], -1) 
+    ([1], -1), 
+    ([1,2,1,1,2,2], 6)
 ])
 def test_minDist(nums: List[int], expected: int): 
     result = Solution().minimumDistance(nums)
