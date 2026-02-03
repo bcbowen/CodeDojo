6f3aedf84c489cc0ae92999f79a61a24c2d921e6
Ben Bowen <bcbowen@gmail.com>
2026-02-02 22:10:54 -0500

    3696 Max dist between unequal words

--- leetcode/Easy/3001-4000/3696_MaxDistanceBetweenUnequalWords.py
+++ leetcode/Easy/3001-4000/3696_MaxDistanceBetweenUnequalWords.py
@@ -5,12 +5,17 @@ class Solution:
     def maxDistance(self, words: List[str]) -> int:
         q = deque()
         q.append((0, len(words) - 1))
-        while q: 
-            start, end = q.popleft() 
-            if words[start] != words[end]: 
-                return end - start + 1
-            if end - start == 1: 
-                break 
-            q.append((start + 1, end))
-            q.append((start, end - 1))
+        word_set = set(words)
+        if len(word_set) > 1:
+
+            while q: 
+                start, end = q.popleft() 
+                if words[start] != words[end]: 
+                    return end - start + 1
+                if end - start == 1: 
+                    break 
+                if start < len(words) - 1: 
+                    q.append((start + 1, end))
+                if end > 0: 
+                    q.append((start, end - 1))
         return 0
