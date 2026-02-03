c22c2e3ef96b1b119b1bd5f4c6ef041944d8e40a
Ben Bowen <bcbowen@gmail.com>
2026-02-03 00:11:01 -0500

    Working on 3640

---
+++ leetcode/Easy/1001-2000/1118_NumberDaysInMonth.py
@@ -0,0 +1,13 @@
+class Solution:
+    def numberOfDays(self, year: int, month: int) -> int:
+        # January, March, may, July, August, October, and December
+        longs = [1, 3, 5, 7, 8, 10, 12]
+        shorts = [4, 6, 9, 11] 
+        if month in longs: 
+            return 31
+        elif month in shorts: 
+            return 30
+        else: 
+            is_leap = (year % 100 != 0 and year % 4 == 0) or year % 400 == 0
+            return 28 if not is_leap else 29
+        
---
+++ leetcode/Hard/3640_TrionicSubarray2.py
@@ -0,0 +1,80 @@
+import pytest
+
+from typing import List
+
+
+class Solution:
+    def maxSumTrionic(self, nums: List[int]) -> int:
+        changes = []
+        is_decreasing = False 
+        is_increasing = False 
+        total = 0
+
+        for i in range(1, len(nums)): 
+            if nums[i] > nums[i - 1]: 
+                if is_decreasing: 
+                    changes.append(total)
+                    total = nums[i]
+                    is_decreasing = False
+                elif not is_increasing: 
+                    
+                    total = nums[i]
+                else: 
+                    total += nums[i]
+                is_increasing = True
+            else: 
+                if is_increasing: 
+                    changes.append(total)
+                    total = nums[i]
+                    is_increasing = False
+                elif not is_decreasing:
+                    total = nums[i]
+                else: 
+                    total += nums[i]
+                is_decreasing = True
+
+        max_sum = -(10**10)
+        for i in range(2, len(nums)): 
+            if i > 0: 
+                max_sum = max(max_sum, sum(nums[i - 2:i + 1]))
+        return max_sum
+
+
+"""
+Example 1:
+
+Input: nums = [0,-2,-1,-3,0,2,-1]
+Output: -4
+Explanation:
+
+Pick l = 1, p = 2, q = 3, r = 5:
+
+nums[l...p] = nums[1...2] = [-2, -1] is strictly increasing (-2 < -1).
+nums[p...q] = nums[2...3] = [-1, -3] is strictly decreasing (-1 > -3)
+nums[q...r] = nums[3...5] = [-3, 0, 2] is strictly increasing (-3 < 0 < 2).
+Sum = (-2) + (-1) + (-3) + 0 + 2 = -4.
+
+Example 2:
+
+Input: nums = [1,4,2,7]
+Output: 14
+Explanation:
+
+Pick l = 0, p = 1, q = 2, r = 3:
+
+nums[l...p] = nums[0...1] = [1, 4] is strictly increasing (1 < 4).
+nums[p...q] = nums[1...2] = [4, 2] is strictly decreasing (4 > 2).
+nums[q...r] = nums[2...3] = [2, 7] is strictly increasing (2 < 7).
+Sum = 1 + 4 + 2 + 7 = 14.
+"""
+@pytest.mark.parametrize("nums, expected", [
+    ([0,-2,-1,-3,0,2,-1], -4),
+    ([1,4,2,7], 14) 
+])
+def test_maxSumTrionic(nums: List[int], expected: int):
+    result = Solution().maxSumTrionic(nums)
+    assert(result == expected)
+
+
+if __name__ == "__main__":
+    pytest.main([__file__]) 
