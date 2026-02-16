e447ef315b268761b0823e824fbb88a15cf059f6
Ben Bowen <bcbowen@gmail.com>
2026-02-16 16:25:56 -0500

    1030 Matrix cells in dist order

---
+++ leetcode/Easy/1001-2000/1030_MatrixCellsInDistanceORder.py
@@ -0,0 +1,12 @@
+class Solution:
+    def allCellsDistOrder(self, rows: int, cols: int, rCenter: int, cCenter: int) -> List[List[int]]:
+        result = [] 
+
+        for row in range(rows):
+            for col in range(cols): 
+                result.append([row, col, abs(rCenter - row) + abs(cCenter - col)])
+
+        result.sort(key=lambda r: r[2])
+
+        return [[r[0], r[1]] for r in result]
+    
