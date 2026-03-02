dc95b4ba5d8e7f4adc7974d9851abcfdcca7e0cd
Ben Bowen <bcbowen@gmail.com>
2026-03-02 00:27:47 -0500

    1438 Longest continuous subarray with diff lte limit

--- leetcode/Medium/1000-5000/1438_LongestContinuousSubarrayWithDiff_LTE_Limit.py
+++ leetcode/Medium/1000-5000/1438_LongestContinuousSubarrayWithDiff_LTE_Limit.py
@@ -6,11 +6,31 @@ from typing import List
 class Solution:
     def longestSubarray(self, nums: List[int], limit: int) -> int:
         increasing = deque() 
-        decreasing = deque
+        decreasing = deque()
+
+        left = ans = 0
+
+        for right in range(len(nums)): 
+            while increasing and nums[right] < increasing[-1]: 
+                increasing.pop()
+            while decreasing and nums[right] > decreasing[-1]: 
+                decreasing.pop() 
+        
+            increasing.append(nums[right])
+            decreasing.append(nums[right])
+
+             # maintain window property
+            while decreasing[0] - increasing[0] > limit:
+                if nums[left] == decreasing[0]:
+                    decreasing.popleft()
+                if nums[left] == increasing[0]:
+                    increasing.popleft()
+                left += 1
+            
+            ans = max(ans, right - left + 1)
 
         
-        
-        return 2
+        return ans
     
 """
 Example 1:
